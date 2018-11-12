4ed4ab389539bf0dc9779415d70640176fc039ca
Marcin Frackiewicz <marcin.jarek.frackiewicz@gmail.com>
2018-11-12 17:01:51 +0100

    Latin in progress

---
+++ src/Latin.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Latin {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    static char[] setString(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter string: ");
+        String string = scanner.toString();
+        replaceSpaces = replaceSpaces.trim();
+        char [] array = replaceSpaces.toCharArray();
+
+        return array;
+    }
+
+    static String ReplaceSpaces
+
+
+
+}

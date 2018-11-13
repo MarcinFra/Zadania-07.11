461cb8dcd80cee88eaf50e190cfbfb6521cd647c
Marcin Frackiewicz <marcin.jarek.frackiewicz@gmail.com>
2018-11-13 12:23:27 +0100

    Latin

--- src/Latin.java
+++ src/Latin.java
@@ -4,21 +4,46 @@ public class Latin {
 
     public static void main(String[] args) {
 
+        char [] latinArray = new char[25];
+        String string = setString();
+        char [] array = ReplaceSpaces(string);
+        latinArray = iteration(latinArray);
 
     }
 
-    static char[] setString(){
+    static String setString(){
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("enter string: ");
         String string = scanner.toString();
+
+        return string;
+    }
+
+    static char[] ReplaceSpaces(String replaceSpaces){
+
         replaceSpaces = replaceSpaces.trim();
         char [] array = replaceSpaces.toCharArray();
 
         return array;
     }
 
-    static String ReplaceSpaces
+    static void createLatinArray(){
+
+        char [] array = new char[25];
+    }
+
+    static char[] iteration(char []array){
+
+        for(int i = 0; i<array.length; i++){
+
+            for(int j = 0; j<array.length; j++){
+
+
+            }
+        }
+        return array;
+    }
 
 
 
